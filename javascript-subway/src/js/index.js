c5d17c139fc4123c0c1112158f127a620a4d391c
HyuuunjuKim <43339385+HyuuunjuKim@users.noreply.github.com>
2021-04-11 15:14:39 +0900

    Delete javascript-subway directory

--- javascript-subway/src/js/index.js
+++
@@ -1,4 +0,0 @@
-import '../css/index.css';
-import router from './router';
-
-router.init();
--- javascript-subway/src/js/pages/lines/templates/lineColorOptionTemplate.js
+++
@@ -1,8 +0,0 @@
-function lineColorOptionTemplate(color, index) {
-  const hasNewLine = (index + 1) % 7 === 0;
-  return `<button type="button" class="color-option bg-${color}" data-color-option="bg-${color}"></button> ${
-    hasNewLine ? '<br/>' : ''
-  }`;
-}
-
-export default lineColorOptionTemplate;
